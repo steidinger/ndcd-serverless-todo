1c0174d3f85cb46a3b0a423aeae6ea8a9336bd65
Frank Steidinger <steidinger@acm.org>
2021-03-04 20:36:25 +0100

    chore: update client config for auth0 and aws

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,8 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const apiId = 'ffdfx353o1'
+export const apiEndpoint = `https://${apiId}.execute-api.eu-central-1.amazonaws.com/dev`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-383jvs9k.eu.auth0.com',            // Auth0 domain
+  clientId: 'EdwtcwhfTnVxPbVmOMMvyrVDuOo7MzhB',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
